8d91b7cdf29dd7b08efbe498ff6c7d2cd6bafaf9
VenujASB <109645464+VenujASB@users.noreply.github.com>
2022-08-21 17:43:38 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
 import asyncio
 from pyrogram import Client
 
-api_id = 12345
-api_hash = "0123456789abcdef0123456789abcdef"
-bot_token = 
+api_id = 18862638
+api_hash = "2a4a8dc0c1f6c9cb65f9f144439558ae"
+bot_token = "5713884943:AAGBn_ESkg3y0VZurKkFnFf2pzbtfnCWSmQ"
 
 async def main():
     async with Client("bot", api_id, api_hash, bot_token) as app:
