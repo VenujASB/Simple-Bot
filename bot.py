876f52c9ba4b42d349dc06781cbc066758890757
VenujASB <109645464+VenujASB@users.noreply.github.com>
2022-08-21 17:46:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,15 @@
-import asyncio
-from pyrogram import Client
+from pyrogram import Client, filters
 
 api_id = 18862638
 api_hash = "2a4a8dc0c1f6c9cb65f9f144439558ae"
 bot_token = "5713884943:AAGBn_ESkg3y0VZurKkFnFf2pzbtfnCWSmQ"
 
-async def main():
-    async with Client("bot", api_id, api_hash, bot_token) as app:
-        await app.send_message("me", "Greetings from **Pyrogram**!")
+app = Client("bot", api_id, api_hash, bot_token)
 
 
-asyncio.run(main())
+@app.on_message(filters.private)
+async def hello(client, message):
+    await message.reply("**Hello Friend!**")
+
+
+app.run()
