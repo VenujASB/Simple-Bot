d579a198a0364d981495e585f5cba65374ce71f7
VenujASB <109645464+VenujASB@users.noreply.github.com>
2022-08-22 18:57:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import os
 from pyrogram import Client, filters
 from config import Config
+import aiofiles
+import aiohttp
 
 bot_token = Config.bot_token
 api_id = Config.api_id
@@ -8,6 +10,14 @@ api_hash = Config.api_hash
 
 bot = Client("bot", api_id, api_hash, bot_token)
 
+async def fetch(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            try:
+                data = await resp.json()
+            except:
+                data = await resp.text()
+    return data
 
 @bot.on_message(filters.regex("hi"))
 async def send_hi(_, message):
