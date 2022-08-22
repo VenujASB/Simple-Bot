56fbae165d35021e63a6d2fb901a189f0d35215b
VenujASB <109645464+VenujASB@users.noreply.github.com>
2022-08-22 15:45:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,6 @@
 from pyrogram import Client, filters
-import aiofiles
-import aiohttp
 import os
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+
 
 api_id = 18862638
 api_hash = "2a4a8dc0c1f6c9cb65f9f144439558ae"
@@ -11,11 +9,11 @@ bot_token = "5713884943:AAGBn_ESkg3y0VZurKkFnFf2pzbtfnCWSmQ"
 app = Client("bot", api_id, api_hash, bot_token)
 
 
-@app.on_message(filters.private)
-async def hello(client, message):
-    await message.reply("**Hello Friend!**"
-)
+@bot.on_message(filters.regex("hi"))
+async def send_hi(_, message):
+    await bot.send_message(
+        chat_id = message.chat.id,
+        text = "Hello!")   
                        
-
-
+print("Bot is alive!")
 app.run()
