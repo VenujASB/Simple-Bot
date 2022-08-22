67b80821f91ad937989fd3e8c9c5076fdd9c160a
VenujASB <109645464+VenujASB@users.noreply.github.com>
2022-08-22 18:52:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
 import os
 from pyrogram import Client, filters
 
-api_id = 18862638
-api_hash = "2a4a8dc0c1f6c9cb65f9f144439558ae"
-bot_token = "5713884943:AAGBn_ESkg3y0VZurKkFnFf2pzbtfnCWSmQ"
+bot_token = Config.bot_token
+api_id = Config.api_id
+api_hash = Config.api_hash
 
 bot = Client("bot", api_id, api_hash, bot_token)
 
